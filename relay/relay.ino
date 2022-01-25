9b89ca3da4cdcf07cac769e43f32888abf7563a5
Christian Navolskyi <c.navolskyi@gmail.com>
2022-01-25 18:14:34 +0100

    toggle lamp with relay

--- relay/relay.ino
+++ relay/relay.ino
@@ -1,15 +1,37 @@
-#define relayPin 2
+
+//#define relayPin 1
+
+int relayPin = 1;
+
+int lampState = 2; // 0 = aus, 1 = fade-in-on, 2 = pulse
 
 void setup() {
   pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
 }
 
 void loop() {
+  if (lampState == 0) {
+    delay(1000);
+    toggleNextState();
+  } else if (lampState == 1) {
+    delay(5000);
+    toggleNextState();
+  } else {
+    toggleNextState();
+  }
+}
+
+void toggleNextState() {
+  Serial.print("current state: ");
+  Serial.print(lampState);
+  Serial.print("\n");
+  
   digitalWrite(relayPin, LOW);
-
-  delay(500);
-
+  delay(100);
   digitalWrite(relayPin, HIGH);
+  delay(100);
+  digitalWrite(relayPin, LOW); 
 
-  delay(500);
+  lampState = (lampState + 1) % 3;
 }
